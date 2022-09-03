0874e5a92d2ae7cb46ad25af91714029c86f0126
kpabn01 <nkpabou@gmail.com>
2022-09-04 01:55:27 +0200

    Initial commit.

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,3 +1,4 @@
+#from crypt import methods
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -8,6 +9,16 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, selection):
+    page = request.args.get("page", 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
@@ -16,16 +27,37 @@ def create_app(test_config=None):
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     """
     @TODO: Use the after_request decorator to set Access-Control-Allow
     """
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
 
     """
     @TODO:
     Create an endpoint to handle GET requests
     for all available categories.
     """
+    @app.route('/categories')
+    def get_categories():
+        selection = Category.query.all()
+        categories = {}
+        if len(selection) == 0:
+            abort(404)
+        else:
+            for category in selection:
+                categories[category.id] = category.type
+
+        return jsonify({
+            "success": True,
+            "categories": categories
+        })
 
 
     """
@@ -40,6 +72,27 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
+    @app.route('/questions')
+    def get_questions():
+        selection = Question.query.order_by(Question.id).all()
+        current_questions = paginate_questions(request, selection)
+        categories = {}
+        for category in Category.query.all():
+            categories[category.id] = category.type
+
+        if len(current_questions) == 0:
+            abort(404)
+
+        return jsonify(
+            {
+                "success": True,
+                "questions": current_questions,
+                "total_questions": len(Question.query.all()),
+                "categories": categories,
+                "currentCategory": categories[current_questions[0]["category"]]
+            }
+        )
+    
 
     """
     @TODO:
@@ -48,6 +101,27 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
+    @app.route('/questions/<int:id>', methods=['DELETE'])
+    def delete_question(id):
+        try:
+            question = Question.query.filter(Question.id == id).one_or_none()
+            if question is None:
+                abort(404)
+            question.delete()
+            selection = Question.query.order_by(Question.id).all()
+            current_questions = paginate_questions(request, selection)
+
+            return jsonify(
+                {
+                    "success": True,
+                    "deleted": id,
+                    "questions": current_questions,
+                    "total_questions": len(Question.query.all()),
+                }
+            )
+        except:
+            abort(422)
+
 
     """
     @TODO:
@@ -59,6 +133,31 @@ def create_app(test_config=None):
     the form will clear and the question will appear at the end of the last page
     of the questions list in the "List" tab.
     """
+    @app.route("/questions", methods=["POST"])
+    def create_question():
+        body = request.get_json()
+
+        new_question = body.get("question", None)
+        new_answer = body.get("answer", None)
+        new_category = body.get("category", None)
+        new_difficulty = body.get("difficulty", None)
+
+        try:
+            question = Question(question=new_question, answer=new_answer, category=new_category, difficulty=new_difficulty)
+            question.insert()
+            selection = Question.query.order_by(Question.id).all()
+            current_books = paginate_questions(request, selection)
+
+            return jsonify(
+                {
+                    "success": True,
+                    "created": question.id,
+                    "books": current_books,
+                    "total_books": len(Question.query.all()),
+                }
+            )
+        except:
+            abort(405)
 
     """
     @TODO:
@@ -70,6 +169,32 @@ def create_app(test_config=None):
     only question that include that string within their question.
     Try using the word "title" to start.
     """
+    @app.route("/books", methods=["POST"])
+    def create_book():
+        body = request.get_json()
+
+        new_title = body.get("title", None)
+        new_author = body.get("author", None)
+        new_rating = body.get("rating", None)
+        search = body.get("search", None)
+
+        try:
+            if search:
+                selection = Book.query.order_by(Book.id).filter(
+                    Book.title.ilike("%{}%".format(search))
+                )
+                # selection = Book.query.order_by(Book.id).filter(or_(Book.title.ilike('%{}%'.format(search)), Book.author.ilike('%{}%'.format(search))))
+                current_books = paginate_books(request, selection)
+
+                return jsonify(
+                    {
+                        "success": True,
+                        "books": current_books,
+                        "total_books": len(selection.all()),
+                    }
+                )
+        except:
+            abort(422)
 
     """
     @TODO:
@@ -97,6 +222,23 @@ def create_app(test_config=None):
     Create error handlers for all expected errors
     including 404 and 422.
     """
+    @app.errorhandler(404)
+    def not_found(error):
+        return (
+            jsonify({"success": False, "error": 404, "message": "resource not found"}),
+            404,
+        )
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return (
+            jsonify({"success": False, "error": 422, "message": "unprocessable"}),
+            422,
+        )
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({"success": False, "error": 400, "message": "bad request"}), 400
 
     return app
 
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,11 +1,17 @@
 import os
+from unicodedata import category
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+from dotenv import load_dotenv
 
+
+load_dotenv()  # take environment variables from .env
+# reading the env variables :
+DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -15,7 +21,9 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}:{}@{}/{}".format("postgres", DATABASE_PASSWORD,
+                             "localhost:5432", self.database_name)
+
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +41,273 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    #----------------------
+    # TESTING CATEGORIES RETRIEVAL
+    #----------------------
+    '''def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["categories"])
+
+    def test_error_getting_categories(self):
+        res = self.client().get('/categories/')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")
+
+    #----------------------
+    # TESTING QUESTIONS RETRIEVAL
+    #----------------------
+    def test_get_paginated_question(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(len(data["questions"]))
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(data["categories"])
+        self.assertTrue(data["currentCategory"])
+
+    def test_404_sent_requesting_beyond_valid_page(self):
+        res = self.client().get('/questions?page=1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")
+
+    #----------------------
+    # TESTING QUESTION DELETION
+    #----------------------
+    def test_delete_a_question(self):
+        res = self.client().delete('/questions/5')
+        data = json.loads(res.data)
+        question = Question.query.filter(Question.id == 5).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertEqual(data['deleted'], 5)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+        self.assertEqual(question, None)
+
+    def test_error_deleting_an_inexisting_question(self):
+        res = self.client().delete('/questions/500')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "unprocessable")'''
+
+    #----------------------
+    # TESTING QUESTION CREATION
+    #----------------------
+    def test_create_new_question(self):
+        res = self.client().post("/questions", json={"question":"night?", "answer": "yes", "category": 3, "difficulty": 1})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["created"])
+        self.assertTrue(len(data["questions"]))
+
+    def test_error_if_question_creation_not_allowed(self):
+        res = self.client().post("/questions/45", json=json={"question":"night?", "answer": "yes", "category": 3, "difficulty": 1})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "method not allowed")
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
+
+    #----------------------
+    # TESTING PAGINATION
+    #----------------------
+
+
+
 
 
 # Make the tests conveniently executable
